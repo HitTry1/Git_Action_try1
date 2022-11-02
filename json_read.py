87cb4580aa5ad7a2ef84bdf4d26dc65a50f9d4dd
Singh <A5913@Axtria.com>
2022-11-02 16:01:32 +0530

    json read  file  code action 1

---
+++ json_read.py
@@ -0,0 +1,20 @@
+import json
+
+json_file=open('DQ1.json','r')
+json_data = json_file.read()
+
+obj = json.loads(json_data)
+
+list = obj['basic']
+print(list)
+print(len(list))
+
+for i int range(len(list)):
+    print("job_name",list[i].get("job_name"))
+    print("job_desc",list[i].get("job_desc"))
+    print("tgt_data_obj",list[i].get("tgt_data_obj"))
+    print("threshold_c",list[i].get("threshold_c"))
+    print("threshold_ce",list[i].get("threshold_ce"))
+
+
+
